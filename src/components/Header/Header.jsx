77a403fdc94331de267c9ab9bdbe1f582fcf249b
Maruku98 <marcparrilla1@gmail.com>
2026-09-07 18:50:28 +0200

    Migrate web to React

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="#brownie">Brownie</a>
+                <a href="#pessic">Pa de pessic</a>
+                <a href="#cookies">Cookies</a>
+                <a href="#santiago">Pastís de Santiago</a>
+                <a href="#cheescake">Cheescake</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
